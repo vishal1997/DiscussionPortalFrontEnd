e9720e654716b7c898303b6ae78288197e2f0035
Vishal <mailtovishal@tatanagar.com>
2017-10-14 23:25:26 +0530

    [Updated Package.json] ''---***Changes in this commit'' [1] This Commit deals with changing package.json.
    ''---***Effects of the change''
    [1] Routes was not working in prod., after the changes error was fixed
    
    ''---***Related Wiki or Issues''
    NA
    
    ''---***Unit Tests''
    NA
    
    ''---***How were the changes checked?''
    The changes were checked via UI:
    http://localhost:3410/user/1541012424

--- Server.js
+++ Server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-//const proxy = require('express-http-proxy');
+const proxy = require('express-http-proxy');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
-//app.use('/api/*', proxy('https://localhost:8080'));
-//app.use('/userlogoutpage', proxy('https://localhost:8080'));
-//app.use('/userloginpage', proxy('https://localhost:8080'));
+app.use('/api/*', proxy('http://18.221.33.176:8080'));
+app.use('/userlogoutpage', proxy('http://18.221.33.176:8080'));
+app.use('/userloginpage', proxy('http://18.221.33.176:8080'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 3410);
